a3b763950747ec159ff72dc5217dc7c8cd541409
Abhishek Saini <abhishek.saini@tyroo.com>
2022-02-05 21:57:23 +0530

    feat: add task and mark complete

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,33 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    event TaskCompleted(uint256 id, bool completed);
+
+    constructor() public {
+        createTask("Check out abhisaini.in");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,138 @@
+App = {
+    loading: false,
+    contracts: {},
+
+    load: async () => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
+
+    loadWeb3: async () => {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            App.web3Provider = window.ethereum;
+            try {
+                // Request account access
+                await window.ethereum.request({ method: "eth_requestAccounts" });;
+            } catch (error) {
+                // User denied account access...
+                console.error("User denied account access")
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = window.web3.currentProvider;
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+            return
+        }
+        web3 = new Web3(App.web3Provider);
+    },
+
+    loadAccount: async () => {
+        // Set the current blockchain account
+        App.account = web3.eth.accounts[0]
+    },
+
+    loadContract: async () => {
+        // Create a JavaScript version of the smart contract
+        const todoList = await $.getJSON('TodoList.json')
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+
+        // Hydrate the smart contract with values from the blockchain
+        App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+        // Prevent double render
+        if (App.loading) {
+            return
+        }
+
+        // Update app loading state
+        App.setLoading(true)
+
+        // Render Account
+        $('#account').html(App.account)
+
+        // Render Tasks
+        await App.renderTasks()
+
+        // Update loading state
+        App.setLoading(false)
+    },
+
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content, { from: App.account })
+        window.location.reload()
+    },
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId, { from: App.account })
+        window.location.reload()
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+            loader.show()
+            content.hide()
+        } else {
+            loader.hide()
+            content.show()
+        }
+    },
+
+    renderTasks: async () => {
+        // Load the total task count from the blockchain
+        const taskCount = await App.todoList.taskCount()
+        const $taskTemplate = $('.taskTemplate')
+
+        // Render out each task with a new task template
+        for (var i = 1; i <= taskCount; i++) {
+            // Fetch the task data from the blockchain
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0].toNumber()
+            const taskContent = task[1]
+            const taskCompleted = task[2]
+
+            // Create the html for the task
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                .prop('name', taskId)
+                .prop('checked', taskCompleted)
+                .on('click', App.toggleCompleted)
+
+            // Put the task in the correct list
+            if (taskCompleted) {
+                $('#completedTaskList').append($newTaskTemplate)
+            } else {
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            // Show the task
+            $newTaskTemplate.show()
+        }
+    },
+
+
+
+
+}
+
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+})
